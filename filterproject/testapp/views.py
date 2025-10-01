9467a8a3b7e10a239780aabfe4691e40f46f349f
Mohd Aman <mohdaman716412b@gmail.com>
2025-10-01 20:25:22 +0530

    second commit by me..

--- filterproject/testapp/views.py
+++ filterproject/testapp/views.py
@@ -4,5 +4,6 @@ from testapp.models import FilterModel
 # Create your views here.
 def upper(req):
     records = FilterModel.objects.all()
+    print('hello world...')
     d = {'records':records}
     return render(req,'testapp/upper.html',d)
